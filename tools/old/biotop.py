55091e8755946e5d1010960fad45e07a48769e2e
Sasha Goldshtein <goldshtn@gmail.com>
2017-03-30 17:28:22 -0400

    biotop: Migrate to kernel tracepoints instead of kprobes
    This commit simplifies biotop by using kernel tracepoints instead
    of kprobes. The block:block_rq_insert, block:block_rq_issue and
    block:block_rq_complete tracepoints are used to track block I/O
    request latency, and aggregate it in the kernel.
    
    This version only works on Linux 4.7, so the old version was moved
    into tools/old.

--- tools/old/biotop.py
+++ tools/old/biotop.py
@@ -12,8 +12,7 @@
 # Copyright 2016 Netflix, Inc.
 # Licensed under the Apache License, Version 2.0 (the "License")
 #
-# 06-Feb-2016   Brendan Gregg     Created this.
-# 03-Apr-2017   Sasha Goldshtein  Migrated to kernel tracepoints.
+# 06-Feb-2016   Brendan Gregg   Created this.
 
 from __future__ import print_function
 from bcc import BPF
@@ -60,13 +59,19 @@ b = BPF(text="""
 #include <uapi/linux/ptrace.h>
 #include <linux/blkdev.h>
 
+// for saving process info by request
+struct who_t {
+    u32 pid;
+    char name[TASK_COMM_LEN];
+};
+
 // the key for the output summary
 struct info_t {
     u32 pid;
-    u32 major;
-    u32 minor;
-    char rwbs[8];
-    char comm[TASK_COMM_LEN];
+    int rwflag;
+    int major;
+    int minor;
+    char name[TASK_COMM_LEN];
 };
 
 // the value of the output summary
@@ -76,97 +81,103 @@ struct val_t {
     u32 io;
 };
 
-// the key for correlating issue and completion
-struct start_key_t {
-    dev_t dev;
-    sector_t sector;
-};
-
-// the value for correlated issue with I/O details
-struct start_val_t {
-    u32 pid;
-    u64 ts;
-    u64 bytes;
-    char comm[TASK_COMM_LEN];
-};
-
-BPF_HASH(start, struct start_key_t, struct start_val_t);
+BPF_HASH(start, struct request *);
+BPF_HASH(whobyreq, struct request *, struct who_t);
 BPF_HASH(counts, struct info_t, struct val_t);
 
-TRACEPOINT_PROBE(block, block_rq_issue)
+// cache PID and comm by-req
+int trace_pid_start(struct pt_regs *ctx, struct request *req)
 {
-    struct start_key_t key = {};
-    key.dev = args->dev;
-    key.sector = args->sector;
+    struct who_t who = {};
 
-    struct start_val_t val = {};
-    val.pid = bpf_get_current_pid_tgid() >> 32;   // process id (not thread)
-    val.ts = bpf_ktime_get_ns();
-    if (args->nr_sector != 0) {
-        val.bytes = args->nr_sector << 9;
-    } else {
-        val.bytes = args->bytes;
+    if (bpf_get_current_comm(&who.name, sizeof(who.name)) == 0) {
+        who.pid = bpf_get_current_pid_tgid();
+        whobyreq.update(&req, &who);
     }
-    bpf_get_current_comm(&val.comm, sizeof(val.comm));
-
-    start.update(&key, &val);
 
     return 0;
 }
 
-TRACEPOINT_PROBE(block, block_rq_complete)
+// time block I/O
+int trace_req_start(struct pt_regs *ctx, struct request *req)
 {
-    struct start_key_t key = {};
-    key.dev = args->dev;
-    key.sector = args->sector;
+    u64 ts;
 
-    struct start_val_t *start_valp;
+    ts = bpf_ktime_get_ns();
+    start.update(&req, &ts);
+
+    return 0;
+}
+
+// output
+int trace_req_completion(struct pt_regs *ctx, struct request *req)
+{
+    u64 *tsp;
 
     // fetch timestamp and calculate delta
-    start_valp = start.lookup(&key);
-    if (start_valp == 0) {
+    tsp = start.lookup(&req);
+    if (tsp == 0) {
         return 0;    // missed tracing issue
     }
 
+    struct who_t *whop;
     struct val_t *valp, zero = {};
-    u64 delta_us = (bpf_ktime_get_ns() - start_valp->ts) / 1000;
+    u64 delta_us = (bpf_ktime_get_ns() - *tsp) / 1000;
 
     // setup info_t key
     struct info_t info = {};
-    info.major = MAJOR(args->dev);
-    info.minor = MINOR(args->dev);
-    info.pid = start_valp->pid;
-    bpf_probe_read(&info.comm, sizeof(info.comm), start_valp->comm);
+    info.major = req->rq_disk->major;
+    info.minor = req->rq_disk->first_minor;
+/*
+ * The following deals with a kernel version change (in mainline 4.7, although
+ * it may be backported to earlier kernels) with how block request write flags
+ * are tested. We handle both pre- and post-change versions here. Please avoid
+ * kernel version tests like this as much as possible: they inflate the code,
+ * test, and maintenance burden.
+ */
+#ifdef REQ_WRITE
+    info.rwflag = !!(req->cmd_flags & REQ_WRITE);
+#elif defined(REQ_OP_SHIFT)
+    info.rwflag = !!((req->cmd_flags >> REQ_OP_SHIFT) == REQ_OP_WRITE);
+#else
+    info.rwflag = !!((req->cmd_flags & REQ_OP_MASK) == REQ_OP_WRITE);
+#endif
 
-    valp = counts.lookup_or_init(&info, &zero);
+    whop = whobyreq.lookup(&req);
+    if (whop == 0) {
+        // missed pid who, save stats as pid 0
+        valp = counts.lookup_or_init(&info, &zero);
+    } else {
+        info.pid = whop->pid;
+        __builtin_memcpy(&info.name, whop->name, sizeof(info.name));
+        valp = counts.lookup_or_init(&info, &zero);
+    }
 
     // save stats
     valp->us += delta_us;
-    valp->bytes += start_valp->bytes;
+    valp->bytes += req->__data_len;
     valp->io++;
 
-    start.delete(&key);
+    start.delete(&req);
+    whobyreq.delete(&req);
 
     return 0;
 }
-""")
-
-diskname_cache = {}
-
-def dev_to_disk(major, minor):
-    if (major, minor) in diskname_cache:
-        return diskname_cache[(major, minor)]
-
-    for line in open("/proc/partitions").readlines():
-        if "major" in line: continue
-        parts = line.strip().split()
-        if len(parts) != 4: continue
-        mj, mn, name = int(parts[0]), int(parts[1]), parts[3]
-        diskname_cache[(mj, mn)] = name
-
-    return diskname_cache.get((major, minor), "[unknown]")
+""", debug=0)
+b.attach_kprobe(event="blk_account_io_start", fn_name="trace_pid_start")
+b.attach_kprobe(event="blk_start_request", fn_name="trace_req_start")
+b.attach_kprobe(event="blk_mq_start_request", fn_name="trace_req_start")
+b.attach_kprobe(event="blk_account_io_completion",
+    fn_name="trace_req_completion")
 
 print('Tracing... Output every %d secs. Hit Ctrl-C to end' % interval)
+
+# cache disk major,minor -> diskname
+disklookup = {}
+with open(diskstats) as stats:
+    for line in stats:
+        a = line.split()
+        disklookup[a[0] + "," + a[1]] = a[2]
 
 # output
 exiting = 0
@@ -192,12 +203,17 @@ while 1:
     for k, v in reversed(sorted(counts.items(),
                                 key=lambda counts: counts[1].bytes)):
 
-        diskname = dev_to_disk(k.major, k.minor)
+        # lookup disk
+        disk = str(k.major) + "," + str(k.minor)
+        if disk in disklookup:
+            diskname = disklookup[disk]
+        else:
+            diskname = "?"
 
         # print line
         avg_ms = (float(v.us) / 1000) / v.io
         print("%-6d %-16s %1s %-3d %-3d %-8s %5s %7s %6.2f" % (k.pid,
-            k.comm.decode(), k.rwbs, k.major, k.minor,
+            k.name.decode(), "W" if k.rwflag else "R", k.major, k.minor,
             diskname, v.io, v.bytes / 1024, avg_ms))
 
         line += 1
