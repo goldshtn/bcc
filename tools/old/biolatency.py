47881dcd14ef84d7c3b1f2afdbf8069e257d69bc
Sasha Goldshtein <goldshtn@gmail.com>
2017-03-30 16:43:47 -0400

    biolatency: Migrate to kernel tracepoints instead of kprobes
    This commit simplifies biolatency by using kernel tracepoints instead
    of kprobes. The block:block_rq_insert, block:block_rq_issue and
    block:block_rq_complete tracepoints are used to track block I/O
    request latency, and aggregate it in the kernel.
    
    This version only works on Linux 4.7, so the old version was moved
    into tools/old.

--- tools/old/biolatency.py
+++ tools/old/biolatency.py
@@ -2,15 +2,14 @@
 # @lint-avoid-python-3-compatibility-imports
 #
 # biolatency    Summarize block device I/O latency as a histogram.
-#               For Linux, uses BCC, eBPF.
+#       For Linux, uses BCC, eBPF.
 #
 # USAGE: biolatency [-h] [-T] [-Q] [-m] [-D] [interval] [count]
 #
 # Copyright (c) 2015 Brendan Gregg.
 # Licensed under the Apache License, Version 2.0 (the "License")
 #
-# 20-Sep-2015   Brendan Gregg     Created this.
-# 03-Apr-2017   Sasha Goldshtein  Migrated to use kernel tracepoints.
+# 20-Sep-2015   Brendan Gregg   Created this.
 
 from __future__ import print_function
 from bcc import BPF
@@ -26,7 +25,7 @@ examples = """examples:
     ./biolatency -D         # show each disk device separately
 """
 parser = argparse.ArgumentParser(
-    description="Summarize block device I/O latency as a histogram.",
+    description="Summarize block device I/O latency as a histogram",
     formatter_class=argparse.RawDescriptionHelpFormatter,
     epilog=examples)
 parser.add_argument("-T", "--timestamp", action="store_true",
@@ -51,37 +50,27 @@ bpf_text = """
 #include <linux/blkdev.h>
 
 typedef struct disk_key {
-    dev_t dev;
+    char disk[DISK_NAME_LEN];
     u64 slot;
 } disk_key_t;
-
-typedef struct start_key {
-    dev_t dev;
-    sector_t sector;
-} start_key_t;
-
-BPF_HASH(start, start_key_t, u64);
+BPF_HASH(start, struct request *);
 STORAGE
 
-TRACEPOINT_PROBE(block, START_PROBE)
+// time block I/O
+int trace_req_start(struct pt_regs *ctx, struct request *req)
 {
-    start_key_t key = {};
-    key.dev = args->dev;
-    key.sector = args->sector;
     u64 ts = bpf_ktime_get_ns();
-    start.update(&key, &ts);
+    start.update(&req, &ts);
     return 0;
 }
 
-TRACEPOINT_PROBE(block, block_rq_complete)
+// output
+int trace_req_completion(struct pt_regs *ctx, struct request *req)
 {
-    start_key_t key = {};
-    key.dev = args->dev;
-    key.sector = args->sector;
     u64 *tsp, delta;
 
     // fetch timestamp and calculate delta
-    tsp = start.lookup(&key);
+    tsp = start.lookup(&req);
     if (tsp == 0) {
         return 0;   // missed issue
     }
@@ -91,7 +80,7 @@ TRACEPOINT_PROBE(block, block_rq_complete)
     // store as histogram
     STORE
 
-    start.delete(&key);
+    start.delete(&req);
     return 0;
 }
 """
@@ -107,37 +96,25 @@ if args.disks:
     bpf_text = bpf_text.replace('STORAGE',
         'BPF_HISTOGRAM(dist, disk_key_t);')
     bpf_text = bpf_text.replace('STORE',
-        'disk_key_t hist = {}; hist.dev = args->dev; ' +
-        'hist.slot = bpf_log2l(delta); dist.increment(hist);')
+        'disk_key_t key = {.slot = bpf_log2l(delta)}; ' +
+        'bpf_probe_read(&key.disk, sizeof(key.disk), ' +
+        'req->rq_disk->disk_name); dist.increment(key);')
 else:
     bpf_text = bpf_text.replace('STORAGE', 'BPF_HISTOGRAM(dist);')
-    bpf_text = bpf_text.replace('STORE', 'dist.increment(bpf_log2l(delta));')
-if args.queued:
-    bpf_text = bpf_text.replace('START_PROBE', 'block_rq_insert')
-else:
-    bpf_text = bpf_text.replace('START_PROBE', 'block_rq_issue')
-
+    bpf_text = bpf_text.replace('STORE',
+        'dist.increment(bpf_log2l(delta));')
 if debug:
     print(bpf_text)
 
 # load BPF program
 b = BPF(text=bpf_text)
-
-diskname_cache = {}
-
-def dev_to_disk(dev):
-    major, minor = dev >> 20, dev & ((1 << 20) - 1)    # like in kdev_t.h
-    if (major, minor) in diskname_cache:
-        return diskname_cache[(major, minor)]
-
-    for line in open("/proc/partitions").readlines():
-        if "major" in line: continue
-        parts = line.strip().split()
-        if len(parts) != 4: continue
-        mj, mn, name = int(parts[0]), int(parts[1]), parts[3]
-        diskname_cache[(mj, mn)] = name
-
-    return diskname_cache.get((major, minor), "[unknown]")
+if args.queued:
+    b.attach_kprobe(event="blk_account_io_start", fn_name="trace_req_start")
+else:
+    b.attach_kprobe(event="blk_start_request", fn_name="trace_req_start")
+    b.attach_kprobe(event="blk_mq_start_request", fn_name="trace_req_start")
+b.attach_kprobe(event="blk_account_io_completion",
+    fn_name="trace_req_completion")
 
 print("Tracing block device I/O... Hit Ctrl-C to end.")
 
@@ -154,7 +131,7 @@ while (1):
     if args.timestamp:
         print("%-8s\n" % strftime("%H:%M:%S"), end="")
 
-    dist.print_log2_hist(label, "disk", section_print_fn=dev_to_disk)
+    dist.print_log2_hist(label, "disk")
     dist.clear()
 
     countdown -= 1
